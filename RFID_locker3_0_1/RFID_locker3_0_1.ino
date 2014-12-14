eeab735a938cace794ef0b3801adb4b7246ff64c
Paweł Pastuszak <kotcrab@gmail.com>
2014-12-14 09:51:27 +0100

    Quick format

--- RFID_locker3_0_1/RFID_locker3_0_1.ino
+++ RFID_locker3_0_1/RFID_locker3_0_1.ino
@@ -1,16 +1,14 @@
 ////////////////////////////////////////////////////////////////////////
 
-
 #include <stdio.h>
 #include <string.h>
 #include <DS1302.h>
 
-
 /* RTC
- CE(DS1302 pin5) -> Arduino D5
- IO(DS1302 pin6) -> Arduino D6
- SCLK(DS1302 pin7) -> Arduino D7
- */
+CE(DS1302 pin5) -> Arduino D5
+IO(DS1302 pin6) -> Arduino D6
+SCLK(DS1302 pin7) -> Arduino D7
+*/
 uint8_t CE_PIN   = 5;
 uint8_t IO_PIN   = 6;
 uint8_t SCLK_PIN = 7;
@@ -21,19 +19,16 @@ char day[10];
 
 String comdata = "";
 int numdata[7] ={
-  0}
+0}
 , j = 0, mark = 0;
 
 DS1302 rtc(CE_PIN, IO_PIN, SCLK_PIN);
-
-
 
 ////////////////////////////////////////////////////////////////////////
 
 #include <Servo.h>
 #include <SPI.h>
 #include <MFRC522.h>
-
 
 #include <SD.h>
 File myFile;
@@ -45,8 +40,7 @@ Servo myservo;
 MFRC522 rfid(SS_PIN, RST_PIN);
 
 unsigned char reading_card[5]; //for reading card
-unsigned char master[5] = {
-  131,142,23,188,0}; // allowed card
+unsigned char master[5] = {131,142,23,188,0}; // allowed card
 unsigned char i;
 
 void indication(int led);
@@ -64,85 +58,85 @@ boolean lock = false;
 
 void setup()
 {
-  myservo.attach(3);
-  Serial.begin(9600);
-  SPI.begin();
-  rfid.PCD_Init();
+	myservo.attach(3);
+	Serial.begin(9600);
+	SPI.begin();
+	rfid.PCD_Init();
 
-  myservo.write(180);
+	myservo.write(180);
 
-  rtc.write_protect(false);
-  rtc.halt(false);
-  pinMode(10, OUTPUT);
-  
- // myFile = SD.open("dane.txt", FILE_WRITE);
- 
-// if (!SD.begin(chipSelect)) {
-//    Serial.println("Card failed, or not present");
-//    // don't do anything more:
-//    return;
-//  }
-  //myFile = SD.open("abc.txt", FILE_WRITE);
- //  myFile.close();
+	rtc.write_protect(false);
+	rtc.halt(false);
+	pinMode(10, OUTPUT);
+	
+	// myFile = SD.open("dane.txt", FILE_WRITE);
+	
+	// if (!SD.begin(chipSelect)) {
+	//    Serial.println("Card failed, or not present");
+	//    // don't do anything more:
+	//    return;
+	//  }
+	//myFile = SD.open("abc.txt", FILE_WRITE);
+	//  myFile.close();
 }
 
 void loop()
 {
-   
-  //Read();
-  //if (rfid.isCard())
+	
+	//Read();
+	//if (rfid.isCard())
 
 
-  // Look for new cards
-  if ( ! rfid.PICC_IsNewCardPresent()) {
-    return;
-  }
-  {
-    if (rfid.PICC_ReadCardSerial())
-    {
-      /* Reading card */
-      //                Serial.println(" ");
-      //                Serial.println("Card found");
-      //                Serial.println("Cardnumber:");
-      for (i = 0; i < 5; i++)
-      {     
+	// Look for new cards
+	if ( ! rfid.PICC_IsNewCardPresent()) {
+		return;
+	}
+	{
+		if (rfid.PICC_ReadCardSerial())
+		{
+			/* Reading card */
+			//                Serial.println(" ");
+			//                Serial.println("Card found");
+			//                Serial.println("Cardnumber:");
+			for (i = 0; i < 5; i++)
+			{
 
-        //    Serial.print(" ");
-        num += rfid.uid.uidByte[i];
-        num += " "; 
-        reading_card[i] = rfid.uid.uidByte[i];
-      }
-      //   Serial.println();
-      //verification
-      for (i = 0; i < 5; i++)
-      {
-        if (reading_card[i]!=master[i])
-        {
-          break;
-        }
-      }
-      if (i == 5)
-      {
-        save();
-        acces = true;
-        allow();
-        
-        save();
+				//    Serial.print(" ");
+				num += rfid.uid.uidByte[i];
+				num += " ";
+				reading_card[i] = rfid.uid.uidByte[i];
+			}
+			//   Serial.println();
+			//verification
+			for (i = 0; i < 5; i++)
+			{
+				if (reading_card[i]!=master[i])
+				{
+					break;
+				}
+			}
+			if (i == 5)
+			{
+				save();
+				acces = true;
+				allow();
+				
+				save();
 
-      }
-      
-      
-      else
-      {
-        acces = false;
-        save();
-        // denied();
-      }
-      //save();
+			}
+			
+			
+			else
+			{
+				acces = false;
+				save();
+				// denied();
+			}
+			//save();
 
-    }
-  }
-  rfid.PICC_HaltA();
+		}
+	}
+	rfid.PICC_HaltA();
 
 
 }
@@ -150,23 +144,23 @@ void loop()
 
 void allow()
 {
-  //Serial.println("Access accept!");
+	//Serial.println("Access accept!");
 
-  if(lock == false){
-    myservo.write(90);
+	if(lock == false){
+		myservo.write(90);
 
-    lock = true;
-  } 
-  else{ 
+		lock = true;
+	}
+	else{
 
-    myservo.write(180);
-    lock = false;
-  }
+		myservo.write(180);
+		lock = false;
+	}
 }
 //void denied()
 //{
 //// Serial.println("Access denied!");
-//  
+//
 //}
 
 
--- RFID_locker3_0_1/features.ino
+++ RFID_locker3_0_1/features.ino
@@ -1,84 +1,84 @@
 String  print_time ()
 {
 
-  Time t = rtc.time();
+	Time t = rtc.time();
 
-  memset(day, 0, sizeof(day));
-  switch (t.day)
-  {
-  case 1: 
-    strcpy(day, "Sunday"); 
-    break;
-  case 2: 
-    strcpy(day, "Monday"); 
-    break;
-  case 3: 
-    strcpy(day, "Tuesday"); 
-    break;
-  case 4: 
-    strcpy(day, "Wednesday"); 
-    break;
-  case 5: 
-    strcpy(day, "Thursday"); 
-    break;
-  case 6: 
-    strcpy(day, "Friday"); 
-    break;
-  case 7: 
-    strcpy(day, "Saturday"); 
-    break;
-  }
+	memset(day, 0, sizeof(day));
+	switch (t.day)
+	{
+		case 1:
+		strcpy(day, "Sunday");
+		break;
+		case 2:
+		strcpy(day, "Monday");
+		break;
+		case 3:
+		strcpy(day, "Tuesday");
+		break;
+		case 4:
+		strcpy(day, "Wednesday");
+		break;
+		case 5:
+		strcpy(day, "Thursday");
+		break;
+		case 6:
+		strcpy(day, "Friday");
+		break;
+		case 7:
+		strcpy(day, "Saturday");
+		break;
+	}
 
-  snprintf(buf, sizeof(buf), "%s %04d-%02d-%02d %02d:%02d:%02d", day, t.yr, t.mon, t.date, t.hr, t.min, t.sec);
+	snprintf(buf, sizeof(buf), "%s %04d-%02d-%02d %02d:%02d:%02d", day, t.yr, t.mon, t.date, t.hr, t.min, t.sec);
 
-  String str(buf);
+	String str(buf);
 
-  return str;
+	return str;
 
-  //"ACCES: " + print_time ()
+	//"ACCES: " + print_time ()
 }
 
 
 void save(){
 
-  // x ++;
+	// x ++;
 
 
-  //if( lock == false ){
+	//if( lock == false ){
 
 
-  if(acces == true && lock == false ){
-   // delay(2500);
-    myFile = SD.open("danel.txt", FILE_WRITE);
+	if(acces == true && lock == false ){
+		// delay(2500);
+		myFile = SD.open("danel.txt", FILE_WRITE);
 
-    if(myFile){
-      String w = "ACCES: " + print_time () ;      
-      myFile.println(w);
-      myFile.close();
-      num = 0;
-    }
-  } 
-   if ( acces == false){
-     delay(2500);
-    myFile = SD.open("teest.txt", FILE_WRITE);
-    if(myFile){
-      String c = "DENIED: " + print_time () + " || Card number: " + num;      
-      myFile.println(c);
-      myFile.close();
-      num = 0;
-    }
-
-
-
-  }
+		if(myFile){
+			String w = "ACCES: " + print_time () ;
+			myFile.println(w);
+			myFile.close();
+			num = 0;
+		}
+	}
+	if ( acces == false){
+		delay(2500);
+		myFile = SD.open("teest.txt", FILE_WRITE);
+		if(myFile){
+			String c = "DENIED: " + print_time () + " || Card number: " + num;
+			myFile.println(c);
+			myFile.close();
+			num = 0;
+		}
 
 
-  //  }
 
-  //  else{
-  //
-  //    x = 0;
-  //  }
+	}
+
+
+	//  }
+
+	//  else{
+	//
+	//    x = 0;
+	//  }
 
 
 }
